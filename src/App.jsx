6b469fcd5c5af94ffc8546fb0ebca37a1794e41e
kim <82406962+KristinaKim6686@users.noreply.github.com>
2022-01-16 22:17:11 +0300

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,57 @@
-// import { render } from '@testing-library/react';
-import React, { Component } from 'react';
-import Section from './components/Section';
-import FeedbackOptions from './components/FeedbackOptions';
-import Notification from './components/Notifications';
-import Statistics from './components/Statistics';
+import React from "react";
+import { useState } from "react";
+import Section from "./components/Section";
+import FeedbackOptions from "./components/FeedbackOptions";
+import Notification from "./components/Notifications";
+import Statistics from "./components/Statistics";
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad
-  };
-  countPositiveFedback = () => {
-    const { good } = this.state;
-    const rate = good / this.countTotalFeedback();
-    return (rate * 100).toFixed(2);
-  }
-  addVote = (e) => {
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const totalFeedback = good + neutral + bad;
+  const positivePercentage = ((good / totalFeedback) * 100).toFixed(2);
+  const controlTitles = ["good", "neutral", "bad"];
+  const addVote = (e) => {
     const { name } = e.target;
-    this.setState((prevState) => {
-      return {
-        [name]: prevState[name] + 1,
-      }
-    });
+    switch (name) {
+      case "good":
+        setGood((prevState) => prevState + 1);
+        break;
+      case "neutral":
+        setNeutral((prevState) => prevState + 1);
+        break;
+      case "bad":
+        setBad((prevState) => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
-  render(){
-  const { good, neutral, bad } = this.state;
-    const controlTitles = Object.keys(this.state);
-    const total = this.countTotalFeedback();
-    const positivePercentage = +this.countPositiveFedback();
+
   return (
     <>
-      <Section title='Leave your feedback please'>
+      <Section title="Leave your feedback please">
         <FeedbackOptions
           options={controlTitles}
-          onLeaveFeedback={this.addVote}>
-        </FeedbackOptions> 
+          onLeaveFeedback={addVote}
+        ></FeedbackOptions>
       </Section>
-      <Section title='Statistics'>
-        {total ? (
+      <Section title="Statistics">
+        {totalFeedback ? (
           <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivePercentage={positivePercentage}>
-          </Statistics>
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          ></Statistics>
         ) : (
-            <Notification message='No feedbacks yet'></Notification>
+          <Notification message="No feedbacks yet"></Notification>
         )}
       </Section>
     </>
-  )
-};
-};
+  );
+}
 
 export default App;
